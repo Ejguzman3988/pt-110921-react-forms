aee8b7db8489a7aba74d79fbe34871ea740937a8
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-07-20 17:31:23 -0400

    Rearranges Post container

--- src/container/PostContainer.js
+++ src/container/PostContainer.js
@@ -5,17 +5,17 @@ class PostContainer extends Component {
     render() {
         return (
             <div id="post-container">
-                <h1>110921 BLOG POST App</h1>
-                <hr></hr>
-                <h3>Form</h3>
-                <Form />
-                <hr></hr>
-                <h3>List</h3>
-                <li>post1</li>
-                <li>post2</li>
-                <li>post3</li>
-                <li>post4</li>
-
+                <h1>110921 BLOG POST App</h1><hr />
+                <div>
+                    <h3>Form</h3>
+                </div><hr />
+                <div>
+                    <h3>List</h3>
+                    <li>post1</li>
+                    <li>post2</li>
+                    <li>post3</li>
+                    <li>post4</li>
+                </div>
             </div>
         )
     }
