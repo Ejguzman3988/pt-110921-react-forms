0fc9d65e6a5e6dfcef14f580b855f4070bc19dfd
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-07-20 12:39:11 -0400

    Build out a few components and containers

---
+++ src/container/PostContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostContainer extends Component {
+    render() {
+        return (
+            <div id="post-container">
+                
+            </div>
+        )
+    }
+}
+
+export default PostContainer
